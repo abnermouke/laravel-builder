787c830496b9bb2b63a74ba97a72905caaa4bf32
Abnermouke <abnermouke@gmail.com>
2020-08-05 16:03:32 +0800

    更改vendor

--- src/LaravelBuilderServiceProvider.php
+++ src/LaravelBuilderServiceProvider.php
@@ -32,6 +32,7 @@ class LaravelBuilderServiceProvider extends ServiceProvider
         // 发布配置文件
         $this->publishes([
             __DIR__.'/../config/builder.php' => config_path('builder.php'),
+            __DIR__.'/../config/project.php' => config_path('project.php'),
             __DIR__.'/../helpers/functions.php' => app_path('Helpers/functions.php'),
             __DIR__.'/../helpers/helpers.php' => app_path('Helpers/helpers.php'),
             __DIR__.'/../helpers/response.php' => app_path('Helpers/response.php'),
